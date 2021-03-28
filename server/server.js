27c1f090615f673f36246b24a6b007051af10bbd
ShadowFax1731 <khare.rachit98@gmail.com>
2021-03-28 08:45:50 +0530

    spotify-clone

---
+++ server/server.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
+const lyricsFinder = require("lyrics-finder");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.redirectUri,
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "a730deff274a4a849db59569776fcb57",
+    clientSecret: "310210191d514d6ca2615832dff590cd",
+  });
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      console.log(data);
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    "No Lyrics Found";
+  res.json({ lyrics });
+});
+
+app.listen(3001);
